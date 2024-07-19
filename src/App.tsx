4105622d9142ff84f84f91aa52f82623e928cd71
prayansh <parmarprayansh2@gmail.com>
2024-07-19 09:17:19 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Dashborad from "@/components/dashborad/dashboard";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full">
+        <Dashborad />
+      </div>
+    </>
+  );
+}
+
+export default App;
